83597ea9491ff089a20d957f5d2cd7ab8cb2bb0c
Holyvert <96556635+Holyvert@users.noreply.github.com>
2023-01-25 15:45:16 -0500

    services for coockies/backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatMenuModule } from '@angular/material/menu';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,16 +11,29 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { CookieService } from 'ngx-cookie-service';
+import { StorageService } from './services/storage.service';
+import { AuthService } from './services/auth.service';
+import { AuthguardGuard } from './services/authguard.guard';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, LandingComponent, NavComponent, FooterComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    NavComponent,
+    FooterComponent,
+    RegisterComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule,MatMenuModule
+    NgbModule,
+    MatMenuModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [CookieService, StorageService, AuthService, AuthguardGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
