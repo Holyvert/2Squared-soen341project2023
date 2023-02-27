2da76813d832fbad7196d566cd8b8cbe32eb33b8
KarinaSandur <karinasd07@hotmail.com>
2023-02-26 23:48:12 -0500

    Updated employer-form functionality

--- src/app/pages/employer-form/employer-form.component.ts
+++ src/app/pages/employer-form/employer-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employer-form',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class EmployerFormComponent {
 
+  employerForm!: FormGroup;
+
+  constructor(
+    private form_builder: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.employerForm = this.form_builder.group({
+      job_title: ['', [Validators.required]],
+      job_location: ['', [Validators.required]],
+      job_location_type: ['', [Validators.required]],
+      salary: ['', [Validators.required]],
+      duration: ['', [Validators.required]],
+      job_description: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+  }
+   
 }
