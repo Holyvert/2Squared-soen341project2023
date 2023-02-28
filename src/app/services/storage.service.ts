4aab453c68f94a7bff490630f6113ad25966e548
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2023-02-28 16:29:06 -0500

    Removed unnecessary comment

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -8,23 +8,6 @@ export class StorageService {
 
   constructor() { }
 
-  // uploadToFirestore(file: any, path: string, storage: Storage): string {
-  //   var url!: string;
-  //   const storageRef = ref_storage(storage, path + file.name);
-  //   const uploadTask = uploadBytesResumable(storageRef, file);
-  //   uploadTask.on('state_changed', 
-  //   (snapshot) => {
-  //     const progress = (snapshot.bytesTransferred / snapshot.totalBytes) / 100;
-  //     console.log('Upload is ' + progress + '% done');
-  //   }
-  //   )
-  //   getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-  //     url = downloadURL;
-  //     console.log('File available at', url);
-  //   });
-  //   return url;
-  // }
-
   async uploadToFirestore(file: any, path: string, storage: Storage): Promise<string> {
     const storageRef = ref_storage(storage, path + file.name);
     const uploadTask = uploadBytesResumable(storageRef, file);
