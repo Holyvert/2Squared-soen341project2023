4ebbb8dad6e83f627afe83aa79e7390b52ef9ec1
Holyvert <96556635+Holyvert@users.noreply.github.com>
2023-03-09 12:25:49 -0500

    #39 Logic
    The back end logic is done but we do not display all the fields yet
    
    Co-Authored-By: Guillaume-Lachapelle <92831445+Guillaume-Lachapelle@users.noreply.github.com>

--- src/app/pages/individual-job-posting/individual-job-posting.component.ts
+++ src/app/pages/individual-job-posting/individual-job-posting.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { onValue } from '@angular/fire/database';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'app-individual-job-posting',
   templateUrl: './individual-job-posting.component.html',
-  styleUrls: ['./individual-job-posting.component.scss']
+  styleUrls: ['./individual-job-posting.component.scss'],
 })
-export class IndividualJobPostingComponent {
+export class IndividualJobPostingComponent implements OnInit {
+  posting!: ParamMap;
+  constructor(private Acrouter: ActivatedRoute, private router: Router) {}
 
+  ngOnInit() {
+    this.posting = this.Acrouter.snapshot.queryParamMap;
+  }
 }
