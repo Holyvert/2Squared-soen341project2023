fa5c2ae97b4934c6253c74b0fce73f669d2da9ec
amczuboka <94947740+amczuboka@users.noreply.github.com>
2023-03-15 13:17:39 -0400

    bug fixes
    Fixed empty post showing up on AppliedTo and changes landing page url to ""

--- src/app/job-post/job-post.component.ts
+++ src/app/job-post/job-post.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import AOS from 'aos';
 import { JobPost } from '../models/user.models';
-import { Database, set, ref, update, onValue, getDatabase, child } from '@angular/fire/database';
-import { Observable } from 'rxjs';
+import { Database, set, ref, onValue, child } from '@angular/fire/database';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -24,42 +23,45 @@ export class JobPostComponent {
   jobDescription: String = 'Knowledge of Angular and TypeScript...';
   searchText: string = '';
   myUser: any = {};
- // isAlone: boolean = false;
 
   jobsArray = [{} as JobPost];
-  soloApplication = {} as JobPost;
 
-  ngOnInit(): void {
+  ngOnInit() {
     AOS.init();
     this.myUser = this.authService.getUser();  
-    if (this.router.url === "/#!") {
-      const starCountRef = ref(this.database, 'job-postings/' );
+    this.jobsArray = [];
+    if (this.router.url === "/" || this.router.url === "/#!") {
+      
+      const starCountRef = ref(this.database, 'job-postings/');
       onValue(starCountRef, (snapshot) => {
       const data = snapshot.val();
       this.jobsArray = ((Object as any).values(data));
+      console.log(this.jobsArray)
       });
 
-    //  this.isAlone = false;
-      console.log(this.jobsArray)
     }
     else {
       const dbRef = ref(this.database);
-      const starCountRef = child(
-        dbRef,
-        `students/${this.myUser.uid}/JobsApplied`
+      const starCountRef = child(dbRef,`students/${this.myUser.uid}/JobsApplied/`
       );
       onValue(starCountRef, (snapshot) => {
       const data = snapshot.val();
-      const keys = Object.keys(data);
-
-    for ( const item of keys){
-    
-    //this.jobsArray.push(this.getApplications(item);)
-    }
-      const values = Object.values(data);
-      
+      console.log(data)
+      const keys =  Object.keys(data);
+        console.log("keys: "+ keys)
+      const dbRef = ref(this.database);
+      keys.forEach(element => {
+        const starCountRef = child(dbRef, `job-postings/${element}` );
+        onValue(starCountRef, (snapshot) => {
+        const data = snapshot.val();
+        this.jobsArray.push(data);
+        console.log("length: "+this.jobsArray.length)
+        console.log(this.jobsArray)
+        });
+      }); 
+      console.log("length: "+this.jobsArray.length)
+      console.log(this.jobsArray)
       });  
-      
     }
   }
   onSearchTextEntered(searchValue: string) {
@@ -67,15 +69,13 @@ export class JobPostComponent {
     console.log('a letter', this.searchText);
   }
 
-  getApplications(id: any) {
+  getApplications(id: any): any {
     const dbRef = ref(this.database);
     const starCountRef = child(dbRef, `job-postings/${id}` );
     onValue(starCountRef, (snapshot) => {
     const data = snapshot.val();
     console.log(data);
-    this.soloApplication = data;
+    return data;
     });
-   // this.isAlone= true;
-    //return somethis
   }
 }
