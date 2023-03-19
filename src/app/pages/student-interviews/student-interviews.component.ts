1b31229acfa4db668be5e9038ed1acb4b7a3208a
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2023-03-19 19:46:52 -0400

    #71 Prevent  some users to access certain pages
    Prevent students to access employers' interview page and vice-versa. Also Added pre-populated fields in user-profile for both employers and students.

--- src/app/pages/student-interviews/student-interviews.component.ts
+++ src/app/pages/student-interviews/student-interviews.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-student-interviews',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-interviews.component.scss']
 })
 export class StudentInterviewsComponent {
+
+  myUser: any = {};
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) {}
+
+
+  ngOnInit(): void {
+    this.myUser = this.authService.getUser();
+    if(this.myUser){
+      if (this.myUser.photoURL == 'Employer') {
+        this.router.navigate([''])
+      }
+    }
+  }
 
 }
