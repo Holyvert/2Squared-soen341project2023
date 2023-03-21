b9bc6e4e1fa3cd85b21d676de393eb8a60bed68d
Jon Micah M- <99670836+MillerSty@users.noreply.github.com>
2023-03-21 15:59:26 -0400

    #54 Add delete button to individual job posting
    Added delete button to individual job posting, with updates to job-post-component to facilitate it
    AMEND: fixed issues with all users deleting job post and requested PR changes

--- src/app/pages/individual-job-posting/individual-job-posting.component.ts
+++ src/app/pages/individual-job-posting/individual-job-posting.component.ts
@@ -1,18 +1,59 @@
-import { onValue } from '@angular/fire/database';
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import {
+  Database,
+  ref,
+  child,
+  remove,
+} from '@angular/fire/database';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-individual-job-posting',
   templateUrl: './individual-job-posting.component.html',
   styleUrls: ['./individual-job-posting.component.scss'],
 })
+
 export class IndividualJobPostingComponent implements OnInit {
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
   posting!: ParamMap;
-  constructor(private Acrouter: ActivatedRoute, private router: Router) {}
+  myUser:any;
+
+  constructor(
+    private Acrouter: ActivatedRoute,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    public authService: AuthService,
+    public database: Database
+    ) {}
 
   ngOnInit() {
+    this.myUser = this.authService.getUser().uid;
     this.posting = this.Acrouter.snapshot.queryParamMap;
-    console.log(this.posting.getAll('Deadline'));
+    console.log(this.Acrouter.snapshot.queryParamMap);
   }
+  onDeleteJobPosting() {
+    const index=this.Acrouter.snapshot.fragment;
+    const dbRef = ref(this.database);
+    remove(child(dbRef, `job-postings/${index}`));
+    alert(`user ${index} was deleted!`);
+    this.router.navigateByUrl("");
+  }
+
+  sendNotification(text: string) {
+    this.snackBar.open(text, '', {
+      duration: 3000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  }
+
 }
