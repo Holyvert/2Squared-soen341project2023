fb11c530c1416736cb6856c9cbff21b7e0f7ebd0
Guillaume-Lachapelle <guillaume2.lachapelle@gmail.com>
2023-03-21 23:12:28 -0400

    Fixed failed test for individual-job-posting

--- src/app/pages/individual-job-posting/individual-job-posting.component.ts
+++ src/app/pages/individual-job-posting/individual-job-posting.component.ts
@@ -1,12 +1,6 @@
-
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import {
-  Database,
-  ref,
-  child,
-  remove,
-} from '@angular/fire/database';
+import { Database, ref, child, remove } from '@angular/fire/database';
 import {
   MatSnackBar,
   MatSnackBarHorizontalPosition,
@@ -14,18 +8,17 @@ import {
 } from '@angular/material/snack-bar';
 import { AuthService } from 'src/app/services/auth.service';
 
-
 @Component({
   selector: 'app-individual-job-posting',
   templateUrl: './individual-job-posting.component.html',
   styleUrls: ['./individual-job-posting.component.scss'],
 })
-
-export class IndividualJobPostingComponent implements OnInit {
+export class IndividualJobPostingComponent {
   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
   posting!: ParamMap;
-  myUser:any;
+  myUser: any;
+  isEmployerWhoPosted: boolean = false;
 
   constructor(
     private Acrouter: ActivatedRoute,
@@ -33,23 +26,27 @@ export class IndividualJobPostingComponent implements OnInit {
     private snackBar: MatSnackBar,
     public authService: AuthService,
     public database: Database
-    ) {}
+  ) {}
 
-  ngOnInit() {
-this.myUser = this.authService.getUser();
-if(this.myUser){
+  ngOnInit(): void {
+    this.myUser = this.authService.getUser();
     this.posting = this.Acrouter.snapshot.queryParamMap;
-    console.log(this.Acrouter.snapshot.queryParamMap);}
+    if (this.myUser && this.posting) {
+      console.log(this.Acrouter.snapshot.queryParamMap);
+      if (this.myUser.uid == this.posting.get('EmployerID'))
+        this.isEmployerWhoPosted = true;
+    }
   }
-  
+
   onDeleteJobPosting() {
-  if(this.myUser){
-    const index=this.Acrouter.snapshot.fragment;
-    const dbRef = ref(this.database);
-    
-    remove(child(dbRef, `job-postings/${index}`));
-    alert(`user ${index} was deleted!`);
-    this.router.navigateByUrl("");}
+    if (this.myUser) {
+      const index = this.Acrouter.snapshot.fragment;
+      const dbRef = ref(this.database);
+
+      remove(child(dbRef, `job-postings/${index}`));
+      this.sendNotification(`user ${index} was deleted!`);
+      this.router.navigate(['']);
+    }
   }
 
   sendNotification(text: string) {
@@ -59,5 +56,4 @@ if(this.myUser){
       verticalPosition: this.verticalPosition,
     });
   }
-
 }
