de5199a89835b6cd735d91d7b4c7c4b1fdb9cb1f
Jon Micah M- <99670836+MillerSty@users.noreply.github.com>
2023-03-24 16:41:36 -0400

    Update 88.cy.ts

--- cypress/e2e/88.cy.ts
+++ cypress/e2e/88.cy.ts
@@ -3,12 +3,10 @@
 // Employer goes to "My Postings"
 // Employer clicks on one of his posts
 // Employer clicks on delete
-// Checks to see that that posting is gone in the database
+// Checks to see that that posting is gone in the database 
+//TODO Check its not in database?
 import { login, enterValueInInputEmployerForm, enterValueInTextAreaEmployerForm, enterValueInMatSelectEmployerForm, enterValueInMatOptionEmployerForm } from './utils.cy'
 describe('Employer Logs In', () => {
-    // it('Enters Credentials of a Student', () => {
-    //   login('isaczu15@gmail.com', 'soen341')
-    //   })
 
 // employer creates new posting for purpose of e2e test on one object
       it('Redirects to Landing Page, checks if is Employer', () => {
@@ -75,10 +73,10 @@ describe('Employer Logs In', () => {
         enterValueInInputEmployerForm(9, 'Montreal')
         enterValueInInputEmployerForm(10, 'Quebec')
         enterValueInInputEmployerForm(11, 'H9J 3B4')
-        // const p = '/Users/ann-marieczuboka/Documents/SOEN 341/2Squared-soen341project2023/cypress/fixtures/Images/photo-1498050108023-c5249f4df085.png'
-        // cy.get('input')
-        //     .eq(12)
-        //     .selectFile(p, {force:true})
+        const p = './cypress/fixtures/Images/2023-03-19_13h43_24.jpg'
+        cy.get('input')
+            .eq(12)
+            .selectFile(p, {force:true})
         cy.get('app-employer-form')
             .find('button')
             .eq(1)
@@ -108,8 +106,4 @@ describe('Employer Logs In', () => {
       cy.wait(4000)
       // cy.pause()
 })
-// const URL="http://localhost:4200/individual?ApplicationMethod=Square&City=d&Company=&Deadline=2023-03-24&Description=d&DocsRequired=No%20Documents&Duration=d&Email=jonmicahmiller@gmail.com&EmployerID=UWRD0FcUhXcdn6LNJl2yytEsCMY2&ID=0cj4vftzs2tt&Image=https:%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fsquared-e7104.appspot.com%2Fo%2Fimages%252F6etdqcxs0zg2023-03-19_13h43_24.jpg%3Falt%3Dmedia%26token%3Df0e1941e-e256-46b9-9130-41cbcb503e1a&JcFirstName=d&JcLastName=d&JobLocation=d&JobLocationType=On-site&JobTitle=Acceptence%20Test%2088&PostalCode=d&Province=d&Requirements=d&Salary=d&Supervisor=d&Website=d"
-
-
-
   })
